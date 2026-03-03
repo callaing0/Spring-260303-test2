cd066e1b789e00d1b18baf58e23a7ba3c07e6a28
callaing0 <0hmong0212@gmail.com>
2026-03-03 13:45:33 +0900

    java/com.book/Entity

---
+++ BoardDTO.java
@@ -0,0 +1,48 @@
+package com.book.Entity;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class BoardDTO {
+	private Long id;
+    private String title;
+    private String content;
+    private String writer;
+    private LocalDateTime regDate;
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public LocalDateTime getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(LocalDateTime regDate) {
+		this.regDate = regDate;
+	}
+    
+    
+
+}
